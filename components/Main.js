5e2e3ea9f1f17de443314dbfc61315a489a88f86
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-04-27 21:03:12 -0300

    Add initial files

---
+++ components/Main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Main() {
+    return (
+        <View style={styles.container}>
+            <Text>Main</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
