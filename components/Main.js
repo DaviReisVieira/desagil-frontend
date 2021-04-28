3c2cb16b39a3b2011ef1b0e21c02aa2f3afd1b76
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-04-28 00:51:53 -0300

    Add props to Main

--- components/Main.js
+++ components/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
-export default function Main() {
+export default function Main(props) {
     return (
         <View style={styles.container}>
             <Text>Main</Text>
