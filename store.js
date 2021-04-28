d88a5486564db0a331d2a3d467e3c326991d7365
Marcelo Hashimoto <marcelo.hashimoto@gmail.com>
2021-04-28 11:55:53 -0300

    Temporarily disable store. (emergency workaround)

--- store.js
+++ store.js
@@ -1,11 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 
-import slices from './slices';
+//import slices from './slices';
 
-const inputs = Object.entries(slices);
+//const inputs = Object.entries(slices);
 
-const outputs = inputs.map(([k, v]) => [k, v.default.reducer]);
+//const outputs = inputs.map(([k, v]) => [k, v.default.reducer]);
 
 export default configureStore({
-    reducer: Object.fromEntries(outputs),
+    //reducer: Object.fromEntries(outputs),
+    reducer: {},
 });
